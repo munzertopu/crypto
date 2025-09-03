54fcefa5fde7ea122673229853a98a45076a03cb
Munzer Topu <munzertopu@gmail.com>
2025-09-03 22:06:41 +0600

    removed ununsed code

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import LottieAnimation from "../components/LottieAnimation";
+import { useEffect, useState } from "react";
 
 interface WelcomePageProps {
   onGetStarted?: () => void;
