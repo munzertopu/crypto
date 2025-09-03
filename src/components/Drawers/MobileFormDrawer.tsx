b0334fe9c835430e929f86241a5053d1a67afa17
Munzer Topu <munzertopu@gmail.com>
2025-09-03 21:26:08 +0600

    fixed sign up button bug

--- src/components/Drawers/MobileFormDrawer.tsx
+++ src/components/Drawers/MobileFormDrawer.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 
 interface DrawerProps {
   isOpen: boolean;
