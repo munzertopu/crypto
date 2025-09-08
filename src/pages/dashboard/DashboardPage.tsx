c5dab15469daf9e99df9c6fcaf88875326485db3
Munzer Topu <munzertopu@gmail.com>
2025-09-09 02:57:58 +0600

    wip mobile dashboard fixing

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -52,7 +52,7 @@ const DashboardPage: React.FC<DashboardPageProps> = ({ onLogout }) => {
       />
 
       {/* Dashboard Content */}
-      <div className="px-4 md:px-10 sm:px-6 md:py-3 md:py-4 w-full">
+      <div className="px-4 md:px-10 sm:px-6  w-full">
         {/* Welcome Banner */}
         {showWelcomeBanner && (
           <>
