74567f6ba9d5f3c88e9dd1f8b092fcf71091ad90
jalal-uddin14 <jalaluddin.csse14@gmail.com>
2025-09-12 18:18:48 +0600

    date range picker issue solved

--- src/components/DateRangePicker.tsx
+++ src/components/DateRangePicker.tsx
@@ -32,8 +32,8 @@ const DateRangePickerPopover: React.FC<DateRangeSelectorProps> = ({
   const [isOpen, setIsOpen] = useState(false);
   const [dateRange, setDateRange] = useState([
     {
-      startDate: selectedDateRange.startDate,
-      endDate: selectedDateRange.endDate,
+      startDate: selectedDateRange.startDate || new Date(),
+      endDate: selectedDateRange.endDate || new Date(),
       key: "selection",
     },
   ]);
@@ -69,7 +69,8 @@ const DateRangePickerPopover: React.FC<DateRangeSelectorProps> = ({
     ];
   };
 
-  const formatDate = (date: Date) => {
+  const formatDate = (date: Date | null) => {
+    if (!date) return "";
     return date.toLocaleDateString("en-US", {
       month: "short",
       day: "numeric",
@@ -78,8 +79,8 @@ const DateRangePickerPopover: React.FC<DateRangeSelectorProps> = ({
   };
 
   const formatDateRange = (selectedDateRange: {
-    startDate: Date;
-    endDate: Date;
+    startDate: Date | null;
+    endDate: Date | null;
   }) => {
     if (!selectedDateRange?.startDate || !selectedDateRange?.endDate) return "";
     const startMonth = selectedDateRange.startDate.toLocaleDateString("en-US", {
@@ -127,8 +128,8 @@ const DateRangePickerPopover: React.FC<DateRangeSelectorProps> = ({
     }
     setDateRange([newSelection]);
     onDateRangeChange({
-      startDate: newSelection.startDate,
-      endDate: newSelection.endDate,
+      startDate: newSelection.startDate || null,
+      endDate: newSelection.endDate || null,
     });
     // setIsOpen(false);
   };
@@ -157,8 +158,8 @@ const DateRangePickerPopover: React.FC<DateRangeSelectorProps> = ({
   useEffect(() => {
     setDateRange([
       {
-        startDate: selectedDateRange.startDate,
-        endDate: selectedDateRange.endDate,
+        startDate: selectedDateRange.startDate || new Date(),
+        endDate: selectedDateRange.endDate || new Date(),
         key: "selection",
       },
     ]);
