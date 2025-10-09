0f09e8c596f80bbec594eee5c7aab07bd6d30333
Munzer Topu <munzertopu@gmail.com>
2025-10-09 23:21:52 +0600

    arrow left icon added

---
+++ src/components/Icons/ArrowLeftIcon.tsx
@@ -0,0 +1,22 @@
+const ArrowLeftIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.5005 16.5999L7.06719 11.1666C6.42552 10.5249 6.42552 9.4749 7.06719 8.83324L12.5005 3.3999"
+        stroke="#7C7C7C"
+        stroke-width="1.2"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowLeftIcon;
