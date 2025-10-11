799ca05129b022771643dd6e2dd1594a455af261
jalal-uddin14 <jalaluddin.csse14@gmail.com>
2025-10-11 18:36:19 +0600

    Table updated

--- src/pages/wallet/components/WalletDetailsPage.tsx
+++ src/pages/wallet/components/WalletDetailsPage.tsx
@@ -220,7 +220,7 @@ const WalletDetailsPage: React.FC<WalletDetailsPageProps> = () => {
                          >
                            <Typography
                               variant="small"
-                              className="flex items-center justify-start gap-2 font-normal text-sm leading-none text-[#666868] dark:text-[#B6B8BA]"
+                              className="flex items-center justify-start gap-2 font-normal text-sm leading-none text-gray-600 dark:text-[#B6B8BA]"
                             >
                               {head}{" "}
                               {head !== "" && (
@@ -270,10 +270,6 @@ const WalletDetailsPage: React.FC<WalletDetailsPageProps> = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                           </svg>
-                           {/* <FontAwesomeIcon 
-                             icon={faEye} 
-                             className="w-6 h-6 mr-4 flex-shrink-0 dark:text-gray-500 text-gray-500 cursor-pointer" 
-                           /> */}
                          </td>
                        </tr>
                      ))}
