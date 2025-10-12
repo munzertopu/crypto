80479e2d7e03693a43b6f88ed14ed5ec5510a735
jalal-uddin14 <jalaluddin.csse14@gmail.com>
2025-10-12 19:51:43 +0600

    Icons updated

--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -44,38 +44,14 @@ const Dropdown: React.FC<DropdownProps> = ({
   }, []);
 
   const ArrowDownSvg = () => (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M13.2797 5.9668L8.93306 10.3135C8.41973 10.8268 7.57973 10.8268 7.06639 10.3135L2.71973 5.9668"
-        stroke="#7C7C7C"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
     </svg>
   );
 
   const ArrowUpSvg = () => (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M13.2797 10.0334L8.93306 5.68676C8.41973 5.17342 7.57973 5.17342 7.06639 5.68676L2.71973 10.0334"
-        stroke="#7C7C7C"
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
     </svg>
   );
 
