87ab8365d9463ca3fbcd7371dd2c6247ff568fc2
Munzer Topu <munzertopu@gmail.com>
2025-10-19 23:14:56 +0600

    icons added

---
+++ src/components/Icons/PencilIcon.tsx
@@ -0,0 +1,36 @@
+const PencilIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="text-gray-500 dark:text-gray-400"
+    >
+      <path
+        d="M8.83958 4.89994L3.36624 10.6933C3.15958 10.9133 2.95958 11.3466 2.91958 11.6466L2.67291 13.8066C2.58624 14.5866 3.14624 15.1199 3.91958 14.9866L6.06624 14.6199C6.36624 14.5666 6.78624 14.3466 6.99291 14.1199L12.4662 8.32661C13.4129 7.32661 13.8396 6.18661 12.3662 4.79327C10.8996 3.41327 9.78624 3.89994 8.83958 4.89994Z"
+        stroke="currentColor"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.92676 5.8667C8.21342 7.7067 9.70676 9.11337 11.5601 9.30003"
+        stroke="currentColor"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2 17.1666H14"
+        stroke="currentColor"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default PencilIcon;
---
+++ src/components/Icons/ThreeDotIcon.tsx
@@ -0,0 +1,39 @@
+const ThreeDotIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="text-gray-500 dark:text-gray-400"
+    >
+      <path
+        d="M9.25 4.99365C9.25 4.57944 9.58579 4.24365 10 4.24365C10.4142 4.24365 10.75 4.57944 10.75 4.99365L10.75 5.00439C10.75 5.41861 10.4142 5.75439 10 5.75439C9.58579 5.75439 9.25 5.41861 9.25 5.00439L9.25 4.99365Z"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.25 9.99487C9.25 9.58066 9.58579 9.24487 10 9.24487C10.4142 9.24487 10.75 9.58066 10.75 9.99487L10.75 10.0056C10.75 10.4198 10.4142 10.7556 10 10.7556C9.58579 10.7556 9.25 10.4198 9.25 10.0056L9.25 9.99487Z"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.25 14.9961C9.25 14.5819 9.58579 14.2461 10 14.2461C10.4142 14.2461 10.75 14.5819 10.75 14.9961L10.75 15.0068C10.75 15.421 10.4142 15.7568 10 15.7568C9.58579 15.7568 9.25 15.421 9.25 15.0068L9.25 14.9961Z"
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ThreeDotIcon;
